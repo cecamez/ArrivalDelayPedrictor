9ad51a9c5cbbffdbff4e624b89ce6c73dbad6b25
vvaquerizo <v.vaquerizo@alumnos.upm.es>
2019-12-14 22:43:58 +0100

    Nuevo proyecto

---
+++ src/main/scala/es/upm/bd/group23/App.scala
@@ -0,0 +1,18 @@
+package es.upm.bd.group23
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.log4j.{Level, Logger}
+
+object App {
+  def main(args : Array[String]) {
+    Logger.getLogger("org").setLevel(Level.WARN)
+    val conf = new SparkConf().setAppName("My first Spark application")
+    val sc = new SparkContext(conf)
+    val data = sc.textFile("file:///tmp/98.txt")
+    val numAs = data.filter(line => line.contains("a")).count()
+    val numBs = data.filter(line => line.contains("b")).count()
+    println(s"Lines with a: ${numAs}, Lines with b: ${numBs}")
+  }
+}
